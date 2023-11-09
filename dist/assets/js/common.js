7ccbb741ffa7ab8bb233130e09a812eb358a22bc
강석민 <dol2156@gmail.com>
2023-11-09 12:10:30 +0900

    upload

---
+++ dist/assets/js/common.js
@@ -0,0 +1,232 @@
+/* 2023-09-13 :: START :: window */
+(() => {
+  // URL 객체 생성
+  const urlObj = new URL(window.location.href);
+
+  // URLSearchParams 객체 생성
+  const params = new URLSearchParams(urlObj.search);
+
+  // 파라메터 정보를 배열로 변환 후, Object.fromEntries를 이용해 객체로 변환
+  const paramsObject = Object.fromEntries(params.entries());
+  window.params = paramsObject;
+  const c_label = 'window.params';
+  const c_label_style = 'border:1px solid black; background:skyblue; color:#333; padding:0.25em 0.5em; font-size:12px; font-weight:bold;';
+  const c_value = JSON.stringify(window.params);
+  const c_value_style = 'border:1px solid black; background:#ffffd4; color:#333; padding:0.25em 0.5em; font-size:12px; border-left:none;';
+  console.log(`%c${c_label}%c${c_value}`, c_label_style, c_value_style);
+
+  // 페이지 타이틀 설정
+  const title = window.params.title;
+  document.title = title;
+})();
+
+/**
+ * 짧은 uid 반환
+ * @returns {string}
+ */
+window.UID = () => {
+  var firstPart = (Math.random() * 46656) | 0;
+  var secondPart = (Math.random() * 46656) | 0;
+  firstPart = ('000' + firstPart.toString(36)).slice(-3);
+  secondPart = ('000' + secondPart.toString(36)).slice(-3);
+  return firstPart + secondPart;
+};
+/* // 2023-09-13 :: END :: window */
+
+/* 2023-10-24 :: START :: setCssVh */
+const setCssVh = () => {
+  document.documentElement.style.setProperty('--vh-100', `${window.innerHeight}px`);
+  document.documentElement.style.setProperty('--vh-50', `${window.innerHeight / 2}px`);
+};
+window.addEventListener('resize', setCssVh);
+setCssVh();
+/* // 2023-10-24 :: END :: setCssVh */
+
+/* 2023-10-21 :: START :: LayerControl */
+const LayerControl = {};
+
+/**
+ *
+ * @param layer_id
+ * @constructor
+ */
+LayerControl.On = (layer_id, callback) => {
+  const el_modal = document.querySelector(`#Modal`);
+  el_modal.classList.add(`On`);
+
+  const el_layer_id = document.querySelector(layer_id);
+  el_layer_id.classList.add(`On`);
+};
+
+/**
+ * @param layer_id
+ * @constructor
+ */
+LayerControl.Off = () => {
+  const el_modal = document.querySelector(`#Modal`);
+  el_modal.classList.remove(`On`);
+
+  const el_layer_list = document.querySelectorAll(`.LayerPopup`);
+  el_layer_list.forEach((el_layer, idx) => {
+    el_layer.classList.remove(`On`);
+  });
+};
+/* // 2023-10-21 :: END :: LayerControl */
+
+/* 2023-10-21 :: START :: ModalControl */
+const ModalControl = {};
+ModalControl.init = () => {
+  const el_modal = document.querySelector(`#Modal`);
+  el_modal.addEventListener('click', (evt) => {
+    LayerControl.Off();
+  });
+};
+/* // 2023-10-21 :: END :: ModalControl */
+
+/**
+ * 스크롤 동작이 끝나면
+ * scroll_end 커스텀 이벤트 발생
+ */
+(() => {
+  let isScrolling;
+  window.addEventListener(
+    'scroll',
+    (e) => {
+      clearTimeout(isScrolling);
+      isScrolling = setTimeout(
+        () => {
+          const event = new Event('scroll_end');
+          window.dispatchEvent(event);
+        },
+        150,
+        false,
+      );
+    },
+    false,
+  );
+})();
+
+/**
+ * DOM 의 class 가 변경되면,
+ * change_class 커스텀 이벤트 발생
+ */
+(() => {
+  // 감지 옵션 (감지할 변경)
+  const config = { attributes: true, subtree: true };
+
+  // 변경 감지 시 실행할 콜백 함수
+  const callback = (mutationList, observer) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+        // class 변경 감지 후 target 으로 이벤트 발송
+        const target = mutation.target;
+        const event = new Event('change_class');
+        target.dispatchEvent(event);
+      }
+    }
+  };
+
+  // 콜백 함수에 연결된 감지기 인스턴스 생성
+  const observer = new MutationObserver(callback);
+
+  // 설정한 변경의 감지 시작
+  observer.observe(document.documentElement, config);
+
+  // 이후 감지 중단 가능
+  // observer.disconnect();
+})();
+
+/**
+ * 자식이 1개일때 자식에게 FixedShell 클래스를 주면
+ * 자식의 포지션이 공중에 뜨더라도 부모의 너비가 유지되도록 유지
+ * <div class="RR relative">
+ *   <div class="FixedShell absolute left-[0] top-[0] z-10">
+ *     ...
+ *   </div>
+ * </div>
+ */
+function updateFixedShell() {
+  const el_fs = [...document.querySelectorAll('.FixedShell')];
+  el_fs.forEach((el, idx) => {
+    const el_parent = el.parentElement;
+    const fs_hei = el.offsetHeight;
+    el_parent.style.setProperty('height', `${fs_hei}px`);
+  });
+}
+
+/**
+ * .StickyBox > .FixedShell 의 DOM 규칙을 갖고
+ * .StickyBox 의 data-sticky-offset 의 값을 읽어와서 Sticky 처리함
+ */
+function updateStickyBox() {
+  const el_list = [...document.querySelectorAll('.StickyBox')];
+
+  el_list.forEach((el, idx) => {
+    checkSticky(el);
+  });
+
+  function checkSticky(target) {
+    const shell = target.querySelector(`:scope > .FixedShell`);
+    if (!shell) return;
+    const box_wid = target.offsetWidth;
+    shell.style.setProperty('width', `${box_wid}px`);
+
+    const y = target.getBoundingClientRect().y;
+    let offset = parseInt(target.getAttribute(`data-sticky-offset`));
+    if (isNaN(offset)) offset = 0;
+
+    if (y >= offset) {
+      target.classList.remove('Sticky');
+      shell.style.removeProperty('position');
+      shell.style.removeProperty('top');
+    } else {
+      target.classList.add('Sticky');
+      shell.style.setProperty('position', 'fixed');
+      shell.style.setProperty('top', `${offset}px`);
+    }
+  }
+}
+
+const UpdateDisplay = () => {
+  updateFixedShell();
+  updateStickyBox();
+};
+
+const initAos = () => {
+  // https://michalsnik.github.io/aos/
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  });
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  ModalControl.init();
+  UpdateDisplay();
+
+  initAos();
+});
+
+window.addEventListener('resize', (evt) => {
+  UpdateDisplay();
+});
+
+window.addEventListener('scroll', (evt) => {
+  UpdateDisplay();
+});
---
+++ dist/assets/lib/handlebars_helper.js
@@ -0,0 +1,462 @@
+Handlebars.logger.level = 'debug';
+
+/**
+ *
+ * @param path
+ * @param render_data
+ */
+Handlebars.write = (path, render_data) => {
+  if (typeof render_data === 'undefined') render_data = {};
+  render_data = Object.assign({ window }, render_data);
+
+  const el_script = document.currentScript;
+
+  const html_str = Handlebars.loadHtml(path);
+
+  //Compile the template
+  const compiled_template = Handlebars.compile(html_str);
+
+  //Render the data into the template
+  let rendered = compiled_template(render_data);
+  rendered = `<!-- ${path} :: START ::  -->` + rendered + `<!-- // ${path} :: END ::  -->`;
+
+  document.write(rendered);
+  el_script.remove();
+};
+
+/**
+ * Handlebars.write 와는 다르게 뒤늦게 넣을때 씀
+ * Handlebars.html('#SiteMapWrap', '/hbs/etc/SiteMapWrap.hbs', response);
+ * @param target_element_selector
+ * @param hbs_path
+ * @param render_data
+ */
+Handlebars.html = (target_element_selector, hbs_path, render_data) => {
+  if (typeof render_data === 'undefined') render_data = {};
+
+  const $target = $(target_element_selector);
+
+  const html_str = Handlebars.loadHtml(hbs_path);
+
+  //Compile the template
+  const compiled_template = Handlebars.compile(html_str);
+
+  //Render the data into the template
+  let rendered = compiled_template(render_data);
+  rendered = `<!-- ${hbs_path} :: START ::  -->` + rendered + `<!-- // ${hbs_path} :: END ::  -->`;
+
+  $target.html(rendered);
+};
+
+/**
+ * 동기 HTML 로드
+ * @param path
+ * @returns {*}
+ * @constructor
+ */
+Handlebars.loadHtml = (path) => {
+  let html_str;
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    /*
+    readyState
+    0	UNSENT	Client has been created. open() not called yet.
+    1	OPENED	open() has been called.
+    2	HEADERS_RECEIVED	send() has been called, and headers and status are available.
+    3	LOADING	Downloading; responseText holds partial data.
+    4	DONE	The operation is complete.
+    */
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        // success
+        html_str = this.responseText;
+      } else {
+        // error
+        const msg = `<div class="flex flex-row items-center justify-center gap-[0] w-full h-full fixed">404 Not Found : ${path}</div>`;
+        console.log(`%c${msg}%c${path}`, 'font-family:D2Coding; border:1px solid black; background:red; color:white; padding:5px; font-size:12px;', 'font-family:D2Coding; background-color:black; border:1px solid black; border-left:none; padding:5px; color:yellow; font-size:12px;');
+        html_str = msg;
+      }
+    }
+  };
+  xhttp.open('GET', path, false);
+  xhttp.send();
+
+  return html_str;
+};
+
+/**
+ * 비동기 xlsx 로드 후 JSON 반환
+ * @param fileUrl
+ * @param callback
+ */
+Handlebars.xlsxToJSON = function (fileUrl, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', fileUrl, true);
+  xhr.responseType = 'arraybuffer';
+
+  xhr.onload = function (e) {
+    var arraybuffer = xhr.response;
+    var data = new Uint8Array(arraybuffer);
+    var workbook = XLSX.read(data, { type: 'array' });
+    var worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    var json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+    // json 가공
+    // 첫번째 row 를 기준으로 비어있는 값에 null 값을 넣어준다.
+    var first_row_data = json[0];
+    var col_len = first_row_data.length;
+
+    json.forEach((obj, idx) => {
+      let i = 0;
+      while (i < col_len) {
+        if (obj[i]) {
+          // console.log(obj[i]);
+        } else {
+          obj[i] = null;
+        }
+        ++i;
+      }
+    });
+
+    let result_array = [];
+    // index 값 기반을 첫번째 row 의 값 기준 데이터로 변환
+    json.forEach((obj, idx) => {
+      const new_obj = {};
+      if (idx != 0) {
+        // console.log(first_row_data);
+        obj.forEach((obj2, idx2) => {
+          new_obj[first_row_data[idx2]] = obj2;
+          //console.log(obj2);
+        });
+
+        result_array.push(new_obj);
+      }
+    });
+
+    callback(result_array);
+  };
+
+  xhr.send();
+};
+
+/************************************************
+ Helper Start
+ *************************************************/
+
+/**
+ * n 회 반복
+ * ex)
+ * {{#LOOP 10}}
+ *   {{index}} {{number}} {{digit}}
+ * {{/LOOP}}
+ */
+Handlebars.registerHelper('LOOP', function (n, block) {
+  var accum = '';
+  for (var i = 0; i < n; ++i) accum += block.fn({ index: i, number: i + 1, digit: (i + 1).toString().padStart(2, '0') });
+  return accum;
+});
+
+/**
+ * 루프
+ * {{#EACH array}} OR {{#EACH (ARR '🍎|🍍|🥝|🍇|🍈')}}
+ *   <div>
+ *     {{obj}}
+ *   </div>
+ * {{/EACH}}
+ */
+Handlebars.registerHelper('EACH', function (data_list, options) {
+  let accum = '';
+  if (arguments.length > 1 && data_list) {
+    //console.log(data_list);
+    data_list.forEach((obj, idx) => {
+      const obj_result = { obj: obj, index: idx, number: idx + 1, digit: (idx + 1).toString().padStart(2, '0') };
+
+      accum += options.fn(obj_result);
+    });
+  }
+  return accum;
+});
+
+/**
+ * String to Array
+ * EACH 와 같이 쓰임
+ * {{#EACH (ARR '🍎|🍍|🥝|🍇|🍈')}}
+ */
+Handlebars.registerHelper('ARR', function (array_str, options) {
+  let arr;
+  if (array_str) {
+    arr = array_str.split('|');
+  } else {
+    arr = false;
+  }
+  return arr;
+});
+
+/**
+ * active_index 에 해당되면 문자열 반환
+ * {{ON 0}}
+ * 또는
+ * {{ON 0 '_on'}}
+ */
+Handlebars.registerHelper('ON', function (active_index, custom_str, options) {
+  const { index } = this;
+  if (typeof custom_str != 'string') custom_str = 'On';
+  const result = active_index === index ? custom_str : '';
+  return result;
+});
+
+/**
+ * 데이터에 \n 이 들어있을때 <br/> 로 치환
+ * ex) {{BR string}}
+ */
+Handlebars.registerHelper('BR', function (text, options) {
+  text = Handlebars.Utils.escapeExpression(text);
+  text = text.replace(/(\r\n|\n|\r)/gm, '<br/>');
+  text = text.replace(/\\n/g, '<br>');
+
+  return new Handlebars.SafeString(text);
+});
+
+/**
+ * 랜덤 난수
+ * 1~10 중에서 랜덤 수
+ * ex) {{RANDOM 1 10 }}
+ */
+Handlebars.registerHelper('RANDOM', function (MIN, MAX) {
+  const k = Math.random() * (MAX - MIN) + MIN;
+  return k;
+});
+
+/**
+ * 랜덤 정수
+ * 1~10 중에서 랜덤 뽑기
+ * ex) {{INT 1 10 }}
+ */
+Handlebars.registerHelper('INT', function (MIN, MAX) {
+  const k = Math.floor(Math.random() * (MAX - MIN + 1) + MIN);
+
+  return k;
+});
+
+/**
+ * 수식과 숫자 x 를 받아서 반환
+ * ex) {{MATH 'x+1' index}}
+ * ex) {{MATH '(x%2===1)?1:(x%6===0)?1:2' number}}
+ */
+Handlebars.registerHelper('MATH', function (mathematics, x) {
+  const result = eval(mathematics);
+  return result;
+});
+
+/**
+ * Object 에 노드 추가
+ * {{VAR this 'NAME' 'ksm'}}
+ */
+Handlebars.registerHelper('VAR', function (object, node_name, value, options) {
+  object[node_name] = value;
+});
+
+/**
+ * 값 합치기
+ * ex) {{ADD this 'NAME' (ADD 'ksm_' @key) }}
+ */
+Handlebars.registerHelper('ADD', function (v1, v2, options) {
+  return v1 + v2;
+});
+
+/**
+ * Partial 사용시에 경로를 동적으로 넣어야하는 경우가 있을때 사용
+ * {{> (PATH obj.partial_path) }}
+ */
+Handlebars.registerHelper('PATH', function (path, options) {
+  if (typeof path !== 'string') {
+    return '';
+  } else {
+    return path;
+  }
+});
+
+/**
+ * 조건문 받아서 Boolean 반환
+ * {{#if (IF this '==' true)}}
+ *   <div>TRUE</div>
+ * {{else}}
+ *   <div>FALSE</div>
+ * {{/if}}
+ */
+Handlebars.registerHelper('IF', function (v1, condition, v2, options) {
+  if (
+    eval(`v1
+    ${condition}
+    v2`)
+  ) {
+    // return options.fn(this);
+    return true;
+  } else {
+    // return options.inverse(this);
+    return false;
+  }
+});
+
+/**
+ * if 문과 함께 사용하고 , condition에 배열 스트링 받아서 포함여부 Boolean 으로 반환
+ * {{#if (CONATIN BTN1 '할인률순,인기순')}}
+ */
+Handlebars.registerHelper('CONATIN', function (p1, condition, options) {
+  let is_contain = false;
+  const arr = condition.split(',');
+
+  let i = 0;
+  let len_i = arr.length;
+  while (i < len_i) {
+    const obj = arr[i].trim();
+    if (obj == p1) {
+      is_contain = true;
+      break;
+    }
+    ++i;
+  }
+  return is_contain;
+});
+
+/**
+ * 기본값 할당
+ * {{DF 'array' '1|2|3'}}
+ */
+Handlebars.registerHelper('DF', function (node_name, value, options) {
+  const object = this;
+
+  if (value.name !== 'DF') {
+    if (typeof object === 'object') {
+      if (typeof object[node_name] === 'undefined') {
+        console.warn(`Handlebars : DF : ${node_name} : ${value}`);
+        object[node_name] = value;
+      } else {
+        // console.warn(`Handlebars : DF : ${node_name}에 이미 값이 할당 되어 있습니다.`);
+        // console.log(`${node_name} == `, object[node_name]);
+      }
+    }
+  } else {
+    // value 가 할당 되지 않았을 경우 경고
+    console.error('Handlebars : DF : value 가 필요합니다.');
+  }
+});
+
+/**
+ * node_에 값이 있나 체크 후 경고
+ * {{NULL_CHECK 'id'}}
+ */
+Handlebars.registerHelper('NULL_CHECK', function (node_name, location_info, options) {
+  const data = this[node_name];
+  if (typeof data === 'undefined') {
+    console.error(`${location_info} : ${node_name} 의 값이 지정되어 있지 않습니다.`);
+  }
+});
+
+/**
+ * root 데이터 있나 없나 체크
+ */
+Handlebars.registerHelper('DV', function (options) {
+  const root = this;
+
+  const len = Object.keys(root).length;
+
+  if (len == 1 && Object.keys(root)[0] == 'window') {
+    return options.fn(this);
+    // return true;
+  } else {
+    return options.inverse(this);
+    // return false;
+  }
+});
+
+/**
+ * root 에 데이터 있나 확인 하고 없으면 node_name 할당
+ * {{SAFE 'array' '1|2|3'}}
+ */
+Handlebars.registerHelper('SAFE', function (node_name, value, options) {
+  if (typeof this[node_name] === 'undefined') {
+    if (arguments.length == 2) {
+      this[node_name] = node_name;
+    }
+
+    if (arguments.length == 3) {
+      this[node_name] = value;
+    }
+  }
+});
+
+/**
+ * 조건 중 하나라도 true 이면 true 반환
+ * {{#if (OR (IF index '==' 0) (IF index '==' 1))}}
+ */
+Handlebars.registerHelper('OR', function (options) {
+  let reslut = false;
+  let boolean_arr = [];
+
+  let i = 0;
+  let len_i = arguments.length - 1;
+  while (i < len_i) {
+    boolean_arr.push(arguments[i]);
+    ++i;
+  }
+
+  boolean_arr.forEach((obj, idx) => {
+    if (obj == true) reslut = true;
+  });
+
+  return reslut;
+});
+
+/**
+ * var_list_str (변수 리스트) 받아서 모두 true 이면 true 반환
+ * {{#AND 'txt1|txt2'}}
+ *   TRUE
+ * {{else}}
+ *   FALSE
+ * {{/AND}}
+ */
+Handlebars.registerHelper('AND', function (var_list_str, options) {
+  if (arguments.length != 2) return false;
+
+  let reslut = true;
+  const var_list = var_list_str.split('|');
+
+  let i = 0;
+  let len_i = var_list.length;
+  while (i < len_i) {
+    const node_name = var_list[i];
+    const v = this[node_name];
+    if (typeof v === 'undefined') {
+      reslut = false;
+      break;
+    }
+    ++i;
+  }
+  console.log(`reslut == `, reslut);
+
+  if (reslut) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+});
+
+/**
+ * 동기식으로 JSON 받아와서 node_name 에 할당 해줌
+ * {{JsonToVar 'SampleData' '/assets/json/SampleData.json'}}
+ */
+Handlebars.registerHelper('JsonToVar', function (node_name, json_url, options) {
+  const json = JSON.parse(Handlebars.loadHtml(json_url));
+  this[node_name] = json;
+});
+
+/**
+ * Object to String
+ * <script>
+ *   Handlebars.write('/hbs/etc/SitemapItem_1-a.hbs', {{objectToJsonStr obj}});
+ * </script>
+ */
+Handlebars.registerHelper('objectToJsonStr', function (obj, options) {
+  return new Handlebars.SafeString(JSON.stringify(obj));
+});
